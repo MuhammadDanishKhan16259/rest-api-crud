a87e24103be7a7a467ff87813c9eca8425edcdd4
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2023-02-26 08:59:55 +0500

    get-delete

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,20 @@ const express = require("express");
 const router = express.Router();
 //posts Model
 const Posts = require("../../models/Posts");
-// @desc GET ALL post
+// @desc GET api/post
+// @desc Create All post
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    if (!posts) throw new Error("No posts found");
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+});
+
+// @routes POST api/posts
+// @desc Create An post
 router.post("/", async (req, res) => {
   const newPost = new Posts(req.body);
   try {
@@ -14,6 +27,28 @@ router.post("/", async (req, res) => {
     res.status(400).json({ msg: err });
   }
 });
+
+// @desc DELETE api/post/:id
+// @desc delete An post
+router.delete("/:id", async (req, res) => {
+  try {
+    const posts = await Posts.findByIdAndDelete(req.params.id);
+    if (!posts) throw Error("No POST FOUND!");
+    res.status(200).json({ Success: true });
+    // res.send(posts);
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+});
+
+// try {
+//   const posts = await Posts.find();
+//   if (!posts) throw new Error("No posts found");
+//   res.status(200).json(posts);
+// } catch (err) {
+//   res.status(400).json({ msg: err });
+// }
+
 // router.get("/", (req, res) => {
 //   //   res.send("lets create post");
 //   console.log(req.body);
